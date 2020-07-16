ab4e50945416f6963a3e68d13d024f874ecf2fba
Marcus McCurdy <mmccurdy@obsidiansecurity.com>
2020-07-16 12:40:15 -0400

    more functional

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # rusty-dice
 
 Silly little app that uses the EFF large word list to generate [diceware](https://en.wikipedia.org/wiki/Diceware) passwords. This is mostly just a small project to help me learn Rust.
+
+# Usage
+
+By default with no arguments dicey will generate a 6 word passphrase.
+    cargo run
+
+
+If you want a longer passphrase provide a single integer argument of the number of words you want.
+
+    cargo run 10
+
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use rand::distributions::Uniform;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let num_words: usize = args[1].parse().unwrap();
+    let args: Vec<String> = env::args().collect(); 
+    let num = match args.as_slice(){
+        [_, arg] => arg,
+        _ => "6",
+    };
+    let num_words: usize = num.parse().unwrap();
     let mut rng = thread_rng();
     let raw_words = include_str!("wordlist.txt");
     let words: Vec<&str> = raw_words.lines().collect();
     let range = Uniform::new(0, words.len());
     for index in (&mut rng).sample_iter(range).take(num_words){
-        println!("word {}", words[index]);
+        println!("{}", words[index]);
     }
 }
