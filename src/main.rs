7175bf6ac0a48a59a2bd1582b88362e03ddac48c
Marcus McCurdy <mmccurdy@obsidiansecurity.com>
2023-10-26 19:43:33 -0400

    use iterators

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use rand::{thread_rng, Rng};
 use rand::distributions::Uniform;
+use rand::{thread_rng, Rng};
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); 
-    let num = match args.as_slice(){
+    let args: Vec<String> = env::args().collect();
+    let num = match args.as_slice() {
         [_, arg] => arg,
         _ => "6",
     };
     let num_words: usize = num.parse().unwrap_or(6);
-    let mut rng = thread_rng();
+    let rng = thread_rng();
     let raw_words = include_str!("wordlist.txt");
     let words: Vec<&str> = raw_words.lines().collect();
     let range = Uniform::new(0, words.len());
-    for index in (&mut rng).sample_iter(range).take(num_words){
-        println!("{}", words[index]);
-    }
+    let pass = rng
+        .sample_iter(range)
+        .take(num_words)
+        .map(|i| words[i])
+        .collect::<Vec<&str>>()
+        .join("-");
+    println!("{}", pass);
 }
